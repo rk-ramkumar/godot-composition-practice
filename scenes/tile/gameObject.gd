42b5af5aaad081f7cd2921e6417be904161cdf6e
rk-ramkumar <rramkumar1306@gmail.com>
2024-03-04 16:52:11 +0530

    Refactored.

--- scenes/tile/gameObject.gd
+++ scenes/tile/gameObject.gd
@@ -1,10 +1,20 @@
 class_name gameObject extends RigidBody3D
 
-@export var width: float = 1
-@export var height: float = 1
 @export var pickable: bool = true
 @export var droppable: bool = true
 @export var color: Color = Color("white")
+@export var width: float = 1:
+	get:
+		return width
+	set(newWidth):
+		width = newWidth
+		_updateBody()
+@export var height: float = 1:
+	get:
+		return height
+	set(newHeight):
+		height = newHeight
+		_updateBody()
 var value:
 	get:
 		return value
@@ -14,4 +24,16 @@ var value:
 var dummy: bool = false
 
 func _updateBody():
+	pass
+
+func _handleDrag(node, cast):
+	position = Vector3(cast.position.x, 2.5, cast.position.z)
+	
+func _on_dragable_drag_move(node, cast):
+	_handleDrag(node, cast)
+ 
+func _on_dragable_drag_start(node):
+	pass
+
+func _on_dragable_drag_stop(node):
 	pass
