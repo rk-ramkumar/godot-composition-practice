ed45444ea764bd414cddf9d5eb3e0e351bd06db8
rk-ramkumar <rramkumar1306@gmail.com>
2024-03-04 15:24:02 +0530

    Refactored.

--- scenes/tile/gameObject.gd
+++ scenes/tile/gameObject.gd
@@ -2,15 +2,16 @@ class_name gameObject extends RigidBody3D
 
 @export var width: float = 1
 @export var height: float = 1
+@export var pickable: bool = true
+@export var droppable: bool = true
+@export var color: Color = Color("white")
 var value:
 	get:
 		return value
 	set(newValue):
 		value = newValue
 		_updateBody()
-@export var pickable: bool = true
-@export var droppable: bool = true
-@export var color: Color = Color("white")
+var dummy: bool = false
 
 func _updateBody():
 	pass
