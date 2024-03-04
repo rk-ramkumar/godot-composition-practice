502316b8fbcb538facf5f61286331749df481847
rk-ramkumar <rramkumar1306@gmail.com>
2024-03-04 13:37:34 +0530

    introduced clock tile.

---
+++ scenes/clockTile.gd
@@ -0,0 +1,14 @@
+class_name clockTile extends tile
+
+
+func _toString(string):
+	return formatTime(string)
+
+func formatTime(input) -> String:
+	var hour = int(input["hour"]) % 12
+	var minute = input["minute"]
+ 
+	var formattedHour = (str(hour) if hour != 0 else "")
+	var formattedMinute = (" " + str(minute) if minute != 0 else "")
+ 
+	return formattedHour + formattedMinute
