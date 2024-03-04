2c8ddbdd43cf5987a8d485086cebefa6161a8364
rk-ramkumar <rramkumar1306@gmail.com>
2024-03-04 13:38:14 +0530

    Added  config for tiles.

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -3,12 +3,20 @@ extends Node3D
 var tileScene = preload("res://scenes/tile/tile.tscn")
 var slotScene = preload("res://scenes/slot/slot.tscn")
 var tileConfig = {
-	value = 10,
+	value = "Hello",
 	width = 2,
 	position = {
 		x = 0,
 		y = 0
-	} 
+	}
+}
+var numberConfig = {
+	value = 13,
+	width = 2,
+	position = {
+		x = -3,
+		y = 0
+	}
 }
 var slotConfig = {
 	value = 10,
@@ -17,8 +25,20 @@ var slotConfig = {
 	position = {
 		x = 0,
 		y = 3
-	} 
+	}
 }
+var clockConfig = {
+	value = {
+		"hour": 1,
+		"minute": 09
+		},
+	width = 2,
+	position = {
+		x = 3,
+		y = 3
+	}
+}
+
 func _ready():
 	var tile = tileScene.instantiate()
 	_setConfig(tile, tileConfig)
@@ -32,6 +52,6 @@ func _ready():
 func _setConfig(node, config):
 	node.value = config.value
 	node.width  = config.width
-	var curPos = config.position 
+	var curPos = config.position
 	node.color =  Color(config.get("color","white"))
 	node.position = Vector3(curPos.x, 4 ,curPos.y)
